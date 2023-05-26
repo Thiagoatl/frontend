3e116a4123ed2419d5e38b1e52cc9a7079fb6df4
Thiago Almeida Trevisani <127760242+Thiagoatl@users.noreply.github.com>
2023-05-26 15:13:19 -0300

    Update Navbar.tsx

--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css'
 
+function navbar()
+
 const AppNavbar: React.FC = () => {
   return (
     <Navbar collapseOnSelect expand="lg" style={{ backgroundColor: '#219EBC' }} variant="dark">
